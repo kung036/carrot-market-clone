cd3e7956e90f6dda1d4db8ccdf3e9a6c449689de
kung036 <sinariari3@gmail.com>
2024-04-30 17:22:11 +0900

    refactor. svelte로 리팩토링

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Main from "./pages/Main.svelte";
+  import Login from "./pages/Login.svelte";
+  import Singup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  // router 설정
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Singup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes} />
+<!-- 
+<main>
+  <div>안녕하세요!</div>
+</main>
+
+<style>
+  div {
+    color : red;
+  }
+</style> -->
---
+++ src/pages/Login.svelte
@@ -0,0 +1,19 @@
+<script
+  src="https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js"
+></script>
+
+<form id="login-form" action="/login" method="POST">
+  <div>로그인</div>
+  <div>
+    <label for="id">아아디</label>
+    <input type="text" id="id" name="id" required />
+  </div>
+  <div>
+    <label for="password">패스워드</label>
+    <input type="password" id="password" name="password" required />
+  </div>
+  <div>
+    <button type="submit">로그인</button>
+  </div>
+  <div id="info"></div>
+</form>
