4e95bb8a1ed96cd33abd4b8ed0e212916d1799a2
kung036 <sinariari3@gmail.com>
2024-05-03 09:44:49 +0900

    feat. 구글 소셜 로그인 구현

--- src/App.svelte
+++ src/App.svelte
@@ -6,18 +6,48 @@
   import Router from "svelte-spa-router";
   import NotFound from "./pages/NotFound.svelte";
   import "./css/style.css";
+  import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { user$ } from "./store";
+  import { onMount } from "svelte";
+
+  let isLoading = true;
+
+  // 토큰을 이용한 소셜 로그인
+  const checkLogin = async () => {
+    const auth = getAuth();
+    const token = localStorage.getItem("token"); // access token
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   // router 설정
   const routes = {
     "/": Main,
-    "/login": Login,
     "/signup": Singup,
     "/write": Write,
     "*": NotFound,
   };
+
+  // 렌더링될 때마다 동작
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <div>로딩 중입니다~</div>
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
 <!-- 
 <main>
   <div>안녕하세요!</div>
